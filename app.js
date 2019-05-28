1a65bf3595f5212450dfa1b196ff486862524723
Robert Holden <robert@robhold.com>
2019-05-28 10:53:55 -0400

    pug install

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const env = process.env.NODE_ENV || 'development';
-const config = require('/config/config.json')[env];
+const config = require('./config/config.json')[env];
 
 var app = express();
 var PORT = process.env.PORT || config.serverPort;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 // Routes
 app.use(express.static("./public"));
--- routes/html/index.js
+++ routes/html/index.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("components/index", function (err, html) {
             res.render("template", {
-                title: "watchPad",
+                title: "Chat",
                 content: html
             });
         });
