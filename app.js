4f328128ae52f3b2684c7f00721bd01f80e965a5
Robert Holden <robert@robhold.com>
2019-05-29 11:34:30 -0400

    added database storage for sessions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 require('dotenv').config()
 const session = require('express-session');
+const MySQLStore = require('connect-mysql')(session)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,20 @@ const PORT = process.env.PORT || 3000;
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: false,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 3,
+        expires: 1000 * 60 * 60 * 24 * 3
+    },
+    store: new MySQLStore({
+        config: {
+            user: process.env.DBUser,
+            password: process.env.DBPassword,
+            database: process.env.DB
+        }
+    })
 }));
 
 app.use(express.urlencoded({ extended: true }));
