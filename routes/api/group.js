493e53439168ec724328e92a193a561a26e7eea4
Robert Holden <robert@robhold.com>
2019-05-29 13:59:37 -0400

    fixed bad catch

--- routes/api/group.js
+++ routes/api/group.js
@@ -40,8 +40,6 @@ module.exports = function (app) {
         }).catch(error => {
             res.status(500).json({error: error})
         });
-    }).catch(error => {
-        res.status(500).json({error: error})
-    });
+    })
 
 };
