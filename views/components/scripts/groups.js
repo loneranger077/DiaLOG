14e41e4610f0096c990460f6ebe1310cce2fc800
RFHold <Robert@Robhold.com>
2019-05-30 00:23:38 -0400

    added group render

--- views/components/scripts/groups.js
+++ views/components/scripts/groups.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
+    const buildGroupJumbotron = (group) =>{
+        const groupJumbotron = $("<div>").addClass("jumbotron jumbotron-fluid")
+        const groupContainer = $("<div>").addClass("container")
+        groupJumbotron.append(groupContainer)
+        groupContainer.append($("<h2>").addClass("display-4").text(group.name))
+        groupContainer.append($("<p>").addClass("lead").text(group.description))
+        return groupJumbotron
+    }
+
+    const buildGroupsContainer = (groups) => {
+        const container = $("#groupsContainer")
+        for (group of groups){
+            container.append($("<div>").addClass("col-4").append(buildGroupJumbotron(group)))
+        }
+    }
+
     $.ajax({
         url: "/api/groups",
         method: "GET"
     }).then(function(response){
         console.log(response);
+        buildGroupsContainer(response.groups)
     }).catch(function(err){
 
     })
