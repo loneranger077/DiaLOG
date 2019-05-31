24d1c2fc4fdc6be2dba04bdc2eea5aec1edd1892
RFHold <Robert@Robhold.com>
2019-05-30 21:10:35 -0400

    add member supporting back and front end

---
+++ routes/api/member.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const sessionHelper = require("../../helpers/session.js")
+
+const db = require("../../models")
+
+module.exports = function (app) {
+
+    app.post("/api/members/:group", function (req, res) {
+        if (!sessionHelper.active(req)) return res.status(400).json({ error: "not logged in" })
+        const member = req.body
+        db.User.findOne({
+            where: {
+                username: member.username
+            }
+        }).then(user => {
+            db.Member.create({
+                user: user.id,
+                group: req.params.group
+            }).then(member => {
+                res.status(200).json({ success: true, member: member.mapData })
+            }).catch(err => {
+                res.status(500).json({ error: error })
+            })
+        }).catch(err => {
+            res.status(500).json({ error: error })
+        })
+    });
+
+    app.get("/api/members/:group", function (req, res) {
+        if (!sessionHelper.active(req)) return res.status(400).json({ error: "not logged in" })
+        const channel = req.body
+        db.Member.findAll({
+            where: {
+                group: req.params.group
+            }
+        }).then(members => {
+            res.status(200).json({
+                success: true, members: members.map(member => {
+                    return member.mapData
+                })
+            })
+        }).catch(err => {
+            res.status(500).json({ error: error })
+        })
+    });
+
+};
